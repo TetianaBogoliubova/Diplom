01f2ce7493984b834562251587f2817fe31ef9ea
Bogoliubova.Tetiana <bogol.tatyana@gmail.com>
2023-12-24 22:01:05 +0100

    D_24.12.23

--- src/main/java/com/bogoliubova/training_service/entity/TypeOfLearning.java
+++ src/main/java/com/bogoliubova/training_service/entity/TypeOfLearning.java
@@ -53,3 +53,16 @@ public class TypeOfLearning {
                 '}';
     }
 }
+
+
+//    To github.com:TetianaBogoliubova/Diplom.git
+//        ! [rejected]        master -> master (fetch first)
+//        error: failed to push some refs to 'github.com:TetianaBogoliubova/Diplom.git'
+//        hint: Updates were rejected because the remote contains work that you do
+//        hint: not have locally. This is usually caused by another repository pushing
+//        hint: to the same ref. You may want to first integrate the remote changes
+//        hint: (e.g., 'git pull ...') before pushing again.
+//        hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+//
+//        user@ThinkPodT430 MINGW64 ~/IdeaProjects/Training_service (master)
+
