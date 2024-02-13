9d9790b7860b06f8e1c992c774cb2dbba5eb8598
Bogoliubova.Tetiana <bogol.tatyana@gmail.com>
2024-02-13 13:06:48 +0100

    D_13.02.24

--- src/main/java/com/bogoliubova/training_service/TrainingServiceApplication.java
+++ src/main/java/com/bogoliubova/training_service/TrainingServiceApplication.java
@@ -2,44 +2,18 @@ package com.bogoliubova.training_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-        //@MapperScan("com.bogoliubova.training_service.mapper")
+@EnableAspectJAutoProxy
+@ComponentScan(value = "com.bogoliubova.training_service")
+@ComponentScan(value =  "com.bogoliubova.training_service.aspect")
 public class TrainingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TrainingServiceApplication.class, args);
     }
-
-
-    //<include file="db/changelog/changes/v0.0.1-SNAPSHOT/RTN-283-create-tables-changelog.xml"/>
 }
 
 
-//    @Override
-//    public AccountDto createAccount(AccountDto accountDto) {
-//        log.info("Create new account.");
-//        Client client = clientRepository.findById(UUID.fromString(accountDto.getClientId()))
-//                .orElseThrow(() -> new NotFoundClientException("Client not found with id " + accountDto.getClientId()));
-//        Account account = accountMapper.mapToEntity(accountDto);
-//        account.setClient(client);
-//        account.setCreatedAt(new Timestamp(System.currentTimeMillis()));
-//        account.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
-//        return accountMapper.mapToDto(accountRepository.save(account));
-//    }
-
-//@ToString.Include(name = "password")
-// private String maskPassword() {
-// return "********";}
-
-//@JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // not for output
-// @ValidPassword	@NotBlank(message = "{validation.field.NotBlank}")
-// private String password;
-
-//@see "Maven Annotation Processing Configuration"
-
-//private static String TEMPLATE =
-//    "^\\p{XDigit}{8}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{12}$";
-
-//private static final String PATTERN =
-//            "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$»;
--- src/main/java/com/bogoliubova/training_service/aspect/LoggingAspect.java
+++ src/main/java/com/bogoliubova/training_service/aspect/LoggingAspect.java
@@ -7,7 +7,8 @@ import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 
 @Aspect
@@ -26,7 +27,7 @@ public class LoggingAspect {
     public void doBeforeController(JoinPoint joinPoint) {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        log.info("NEW REQUEST:\n" +
+        log.info("***************************************************NEW REQUEST:\n" +
                         "IP : {}\n" +
                         "URL : {}\n" +
                         "HTTP_METHOD : {}\n" +
@@ -40,18 +41,30 @@ public class LoggingAspect {
 
     @Before("serviceLog()")
     public void doBeforeService(JoinPoint joinPoint) {
-        log.info("RUN SERVICE:\n" + "SERVICE_METHOD : {}.{}",
+        log.info("***************************************RUN SERVICE:\n" + "SERVICE_METHOD : {}.{}",
                 joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
     }
 
     @AfterReturning(returning = "returnObject", pointcut = "controllerLog()")
     public void doAfterReturning(Object returnObject) {
-        log.info("\nReturn value: {}\n" + "END OF REQUEST", returnObject);
+        log.info("\n*********************************************Return value from controller: {}\n" + "END OF CONTROLLER REQUEST", returnObject);
     }
 
     @AfterThrowing(throwing = "ex", pointcut = "controllerLog()")
     public void throwException(JoinPoint joinPoint, Exception ex) {
-        log.error("Request throw an exception. Cause - {}. {}",
+        log.error("**************************************************Controller request throw an exception. Cause - {}. {}",
                 Arrays.toString(joinPoint.getArgs()), ex.getMessage());
     }
+
+    @AfterReturning(returning = "returnObject", pointcut = "serviceLog()")
+    public void doAfterReturningService(Object returnObject) {
+        log.info("\n*****************************************************Return value from service: {}\n" + "END OF SERVICE METHOD", returnObject);
+    }
+
+    @AfterThrowing(throwing = "ex", pointcut = "serviceLog()")
+    public void throwExceptionService(JoinPoint joinPoint, Exception ex) {
+        log.error("**********************************************************Service method throw an exception. Cause - {}. {}",
+                Arrays.toString(joinPoint.getArgs()), ex.getMessage());
+    }
+
 }
