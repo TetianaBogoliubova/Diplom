bde67c31531a2231f7863f1bc0e6276de56194df
Bogoliubova.Tetiana <bogol.tatyana@gmail.com>
2024-02-18 13:06:25 +0100

    D_18.02.24

--- src/main/java/com/bogoliubova/training_service/aspect/LoggingAspect.java
+++ src/main/java/com/bogoliubova/training_service/aspect/LoggingAspect.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -12,6 +14,7 @@ import java.util.Arrays;
 @Aspect
 @Component
 @Slf4j
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class LoggingAspect {
     @Pointcut("execution(public * com.bogoliubova.training_service.controller.*.*(..))")
     public void controllerLog() {
