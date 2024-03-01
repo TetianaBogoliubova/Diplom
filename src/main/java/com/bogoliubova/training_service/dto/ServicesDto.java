2a11744b053318b0fd25e11767ecd66984f86faa
Bogoliubova.Tetiana <bogol.tatyana@gmail.com>
2024-03-01 20:49:00 +0100

    D_01.03.24

--- src/main/java/com/bogoliubova/training_service/dto/ServicesDto.java
+++ src/main/java/com/bogoliubova/training_service/dto/ServicesDto.java
@@ -3,10 +3,12 @@ package com.bogoliubova.training_service.dto;
 import lombok.Data;
 import lombok.Value;
 
+import java.math.BigDecimal;
+
 @Value
 //@Data
 public class ServicesDto {
     String type;
 
-    Double servicePrice;
+    BigDecimal servicePrice;
 }
--- src/main/java/com/bogoliubova/training_service/mapper/ServicesMapper.java
+++ src/main/java/com/bogoliubova/training_service/mapper/ServicesMapper.java
@@ -3,10 +3,14 @@ package com.bogoliubova.training_service.mapper;
 import com.bogoliubova.training_service.dto.ServicesDto;
 import com.bogoliubova.training_service.entity.Services;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ServicesMapper {
-    ServicesDto toDto(Services referenceById);
+
+    @Mapping(target = "servicePrice", ignore = true)
+    @Mapping(target = "type", ignore = true)
+    ServicesDto toDto(Services services);
 
     Services toEntity(ServicesDto servicesDto);
 }
